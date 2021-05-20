53735a93fb5bf9deacceada67c46532bae6b4af5
JOSE RICARDO RUGERIO SAN VICENTE <jrugeriosanvicente@tenaris.com>
2021-05-19 19:36:16 -0500

    ADDING BLOCKS AND TEST

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -3,15 +3,23 @@ function Blockchain(){
     this.newTransactions = [];
 }
 
-Blockchain.prototype.createNewBlock = function (nonce, previousHash, hash)
+Blockchain.prototype.createNewBlock = function (nonce, previousBlokcHash, hash)
 {
     const newBlock = {
         index: this.chain.length + 1,
         timestamp : Date.now(),
         transactions: this.newTransactions,
-        nonce = nonce,
-        previousHash = previousHash,
-        hash    = hash
-    }
+        nonce : nonce,
+        hash   : hash,
+        previousBlokcHash : previousBlokcHash,
+    };
+
+    this.newTransactions = [];
+    this.chain.push(newBlock);
+
+    return newBlock;
 
 }
+
+
+module.exports = Blockchain;
---
+++ dev/test.js
@@ -0,0 +1,8 @@
+const BlockChain = require('./blockchain');
+
+const bitcoin = new BlockChain();
+bitcoin.createNewBlock(2565,'JSJAISAI85','67HSGSA77882')
+bitcoin.createNewBlock(2566,'JSJAISAI86','67HSGSA77887')
+bitcoin.createNewBlock(2567,'JSJAISAI87','67HSGSA77888')
+
+console.log(bitcoin);
